87fc2930205a047f7cbb92b6e7561717033cf647
Simona Grieco <simona.grc99@gmail.com>
2025-12-03 09:25:50 +0100

    Add Open Graph meta tags and site favicon for improved SEO and branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import Logo from '../assets/Logo.svg';
+import Logo from '../../public/Logo.svg';
 import {Link} from "react-router-dom";
 
 export default function Navbar() {
@@ -9,7 +9,7 @@ export default function Navbar() {
                     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src={Logo} className="h-10" alt="Little Lemon's Logo"/>
                     </a>
-                    <div className="hidden md:ml-6 md:flex md:space-x-8items-center justify-start w-full md:w-auto md:order-1">
+                    <div className="hidden md:ml-6 md:flex md:space-x-8items-center justify-start w-full md:w-auto">
                         <ul className="flex flex-col font-heading p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                             <li>
                                 <Link to="/" className="block py-2 px-3 rounded-sm md:p-0">Home</Link>
